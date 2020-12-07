984dd23329ed9fbfefd1f657fa5047e8b3c7cbd3
eric <eric.n@onther.io>
2020-12-07 16:44:27 +0900

    front commit

--- src/js/app.js
+++ src/js/app.js
@@ -3,19 +3,53 @@ App = {
   contracts: {},
 	
   init: function() {
-   
+   $.getJSON('../real-estate.json', function(data) {
+     var list = $('#list');
+     var template = $('#template');
+
+     for(i=0; i<data.length; i++){
+       template.find('img').attr('src', data[i].picture);
+       template.find('.id').text(data[i].id);
+       template.find('.type').text(data[i].type);
+       template.find('.area').text(data[i].area);
+       template.find('.price').text(data[i].price);
+
+       list.append(template.html());
+     }
+   })
+
+   return App.initWeb3();
   },
 
   initWeb3: function() {
-	
+	if(typeof web3 !== 'undefined'){
+    App.web3Provider = web3.currentProvider;
+    web3 = new Web3(web3.currentProvider);
+  }else{
+    App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
+    web3 = new Web3(App.web3Provider);
+  }
+
+    return App.initContract();
   },
 
   initContract: function() {
-		
+		$.getJSON('RealEstate.json', function(data){
+      App.contracts.RealEstate = TruffleContract(data);
+      App.contracts.RealEstate.setProvider(App.web3Provider);
+    })
   },
 
   buyRealEstate: function() {	
+    var id = $('#id').val();
+    var name = $('#name').val();
+    var price = $('#price').val();
+    var age = $('#age').val();
 
+    console.log(id);
+    console.log(name);
+    console.log(price);
+    console.log(age);
   },
 
   loadRealEstates: function() {
@@ -30,5 +64,13 @@ App = {
 $(function() {
   $(window).load(function() {
     App.init();
+  });
+
+  $('#buyModal').on('show.bs.modal', function(e){
+    var id = $(e.relatedTarget).parent().find('.id').text();
+    var price = web3.toWei(parseFloat($(e.relatedTarget).parent().find('.price').text() || 0), "ether");
+    
+    $(e.currentTarget).find('#id').val(id);
+    $(e.currentTarget).find('#price').val(price);
   });
 });
