ee7f87af9365461b4b4ec93768721b8cfde09096
eric <eric.n@onther.io>
2020-12-08 17:28:23 +0900

    remix migrate

--- src/js/app.js
+++ src/js/app.js
@@ -38,6 +38,7 @@ App = {
 		$.getJSON('RealEstate.json', function(data){
       App.contracts.RealEstate = TruffleContract(data);
       App.contracts.RealEstate.setProvider(App.web3Provider);
+      App.listenToEvents();
     })
   },
 
@@ -68,11 +69,45 @@ App = {
   },
 
   loadRealEstates: function() {
-	
+    App.contracts.RealEstate.deployed().then(function(instance){
+      return instance.getAllBuyers.call();
+    }).then(function(buyers){
+      for(i=0; i<buyers.length; i++){
+        if(buyers[i] !== '0x0000000000000000000000000000000000000000'){
+          var imgType = $('.panel-realEstate').eq(i).find('img').attr('src').substr(7);
+
+          switch(imgType){
+            case 'apartment.jpg':
+              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/apartment_sold.jpg');
+              break;
+            case 'townhouse.jpg':
+              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/townhouse_sold.jpg');
+              break;
+            case 'house.jpg':
+              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/house_sold.jpg');
+              break;
+          }
+          
+          $('.panel-realEstate').eq(i).find('.btn-buy').text('매각').attr('disabled', true);
+          $('.panel-realEstate').eq(i).find('.btn-buyerInfo').removeAttr('style');
+        }
+      }
+    }).catch(function(err){
+      console.log(err.message);
+    })
   },
 	
   listenToEvents: function() {
-	
+    App.contracts.RealEstate.deployed().then(function(instance){
+      instance.LogBuyRealEstate({}, { fromBlock: 0, toBlock: 'latest' }).watch(function(error, event){
+        if(!error){
+          $('#events').append('<p>' + event.args._buyer + ' 계정에서 ' + event.args._id + ' 번 매물을 매입했습니다.' + '</p>')
+        } else {
+          console.error(error);
+        }
+        App.loadRealEstates();
+      })
+    })
   }
 };
 
@@ -87,5 +122,19 @@ $(function() {
     
     $(e.currentTarget).find('#id').val(id);
     $(e.currentTarget).find('#price').val(price);
+  });
+
+  $('#buyerInfoModal').on('show.bs.modal', function(e){
+    var id = $(e.relatedTarget).parent().find('.id').text();
+    
+    App.contracts.RealEstate.deployed().then(function(instance){
+      return instance.getBuyerInfo.call(id);
+    }).then(function(buyerInfo){
+      $(e.currentTarget).find('#buyerAddress').text(buyerInfo[0]);
+      $(e.currentTarget).find('#buyerName').text(utf8.decode(web3.toUtf8(buyerInfo[1])));
+      $(e.currentTarget).find('#buyerAge').text(buyerInfo[2]);
+    }).catch(function(err){
+      console.log(err.message);
+    });
   });
 });
--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "property common license code trigger liquid science mother swamp reopen file scale";
+
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -6,6 +9,14 @@ module.exports = {
                host: "127.0.0.1",
                port: 8545,
                network_id: "*" // Match any network id
+          },
+          ropsten: {
+               provider: function(){
+                    return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/d24fd15fbb9541ffac7a558172e9705a')
+               },
+               network_id: '3',
+               gas: 450000,
+               gasPrice: 10000000000,
           }
      }
 };
